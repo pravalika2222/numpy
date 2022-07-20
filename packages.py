9914117d98864cc2539266e47fcf3e6a21b4f488
pravalika2222 <107530566+pravalika2222@users.noreply.github.com>
2022-07-20 12:50:14 +0530

    Add files via upload

---
+++ packages.py
@@ -0,0 +1,52 @@
+"""numpy"""
+import numpy as np
+# a=np.array([[2,4,6] ,
+#             [1,3,5]])
+# b=np.array([[1,4,3] ,
+#             [1,2,3]])
+# c=a+b    #[[3,8,9]
+           #[2,5,8]]
+# d=a*b     #[[2,16,18]
+            #[1,6,15]]
+# e=a-b       #[[1,0,3]
+              #[0,1,2]]
+# f=3*a        #[[6,12,18]
+             # [3,9,15]]
+# print(c)
+# print(d)
+# print(e)
+# print(f)  
+#   
+"identity"    
+'''identity means diagonals are same  and attribute 'i' is not defined in numpy'''
+
+# p=np.eye(2)   
+# r=np.eye(2,dtype=int)
+# q=np.eye(2,dtype=bool)
+# print(p)       # [[1.0.]
+                 #[0.1.]]
+# print(r)         #[[1 0]                 # it is in the form of "int"
+                   #[0 1]]
+# print(q)          # [[ True False]       # it is in the form of "bool"
+                   # [ False True]]
+
+# y=np.array([x for x in range(9)])       # [[0 1 2]
+# i=y.reshape(3,3)                         #  [3 4 5]
+# print(i)                                  # [6 7 8]]
+
+# p=np.array([[1.2,2.2,1.1],              # [[1 2 1]]
+#             [1.0,2.1,1.3]])               # [1 2 1]]
+# k=p.astype('int')
+# print(k)
+
+# b=np.array([[1,0,1],               #[[True False True]
+#             [0,0,1],               #[False False True]
+#             [1,1,0]])              #[True True False]]
+# a=b.astype(bool)
+# print(a)
+
+
+
+
+
+
